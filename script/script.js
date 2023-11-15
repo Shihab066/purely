6615a74a4db6e99d4b5d029ac21f1815c7209f90
Shihab066 <md.shihab066@gmail.com>
2023-11-15 14:38:46 +0600

    slider section added and responsive done

--- script/script.js
+++ script/script.js
@@ -1,26 +1,32 @@
 
+//this script is used to manipulate hamburger menu
+
 const navToggle = document.getElementById('nav-toggler');
+// const navMenu = document.getElementById('nav-menu');
+const menuBackground = document.getElementById('menu-background');
+const navMenuItem = document.getElementById('nav-menu-item');
+const toggleOverflow = document.body;
 let showMenu = false;
 
 
 navToggle.addEventListener('click', function () {
     showMenu = !showMenu;
-    const navMenu = document.getElementById('nav-menu');
-    const menuBackground = document.getElementById('menu-background');
-    const navMenuItem = document.getElementById('nav-menu-item');
-    const toggleOverflow = document.body;
-   
+    menuToggle()
+})
 
+menuBackground.addEventListener('click', function () {
+    navToggle.click()
+})
+
+function menuToggle() {
     if (showMenu) {
-        // navMenu.style.display = 'block';
         menuBackground.style.display = 'block';
         navMenuItem.style.transform = 'translateX(0)';
         toggleOverflow.style.overflow = 'hidden';
     }
     else {
-        // navMenu.style.display = 'none';
         menuBackground.style.display = 'none';
         navMenuItem.style.transform = 'translateX(-100%)';
         toggleOverflow.style.overflow = 'auto';
     }
-})
+}
--- script/slider.js
+++ script/slider.js
@@ -1,51 +1,63 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const slider = document.querySelector('.slider');
-    const cards = document.querySelector('.cards');
-    const indicatorsContainer = document.querySelector('.indicators');
-  
-    const cardCount = document.querySelectorAll('.card').length;  
-    const cardWidth = 300;
-  
-    let currentIndex = 0;
-  
-    // Create indicators
-    for (let i = 0; i <= cardCount-4; i++) {
-      const indicator = document.createElement('div');
-      indicator.classList.add('indicator');
-      indicatorsContainer.appendChild(indicator);
-  
-      indicator.addEventListener('click', function () {
-        currentIndex = i;
-        updateSlider();
-      });
-    }
-  
-    const indicators = document.querySelectorAll('.indicator');
-  
-    function updateSlider() {
-      const translateValue = -currentIndex * cardWidth;
-      slider.style.transform = `translateX(${translateValue}px)`;
-  
-      // Update active indicator
-      indicators.forEach((indicator, index) => {
-        indicator.classList.toggle('active', index === currentIndex);
-      });
-    }
-  
-    function nextSlide() {
-    if (currentIndex < cardCount - 4) {
-        currentIndex++        
+  const slider = document.querySelector('.slider');
+  const cards = document.querySelector('.cards');
+  const indicatorsContainer = document.querySelector('.indicators');
+  const cardCount = document.querySelectorAll('.card').length;
+  const card = document.querySelector('.card').clientWidth;
+  const cardGap = window.getComputedStyle(cards).gap;
+  const cardGapValue = parseInt(cardGap.replace(/\D/g, ''));
+  const cardWidth = card + cardGapValue;
+  const bodyWidth = document.body.clientWidth;
+  let cardPerView = 4;
+
+  if (bodyWidth <= 576 && bodyWidth > 374) {
+    cardPerView = 3;
+  }
+
+  else if (bodyWidth <= 374) {
+    cardPerView = 2;
+  }
+
+  let currentIndex = 0;
+
+  // Create indicators
+  for (let i = 0; i <= cardCount - cardPerView; i++) {
+    const indicator = document.createElement('div');
+    indicator.classList.add('indicator');
+    indicatorsContainer.appendChild(indicator);
+
+    indicator.addEventListener('click', function () {
+      currentIndex = i;
+      updateSlider();
+    });
+  }
+
+  const indicators = document.querySelectorAll('.indicator');
+
+  function updateSlider() {
+    const translateValue = -currentIndex * cardWidth;
+    slider.style.transform = `translateX(${translateValue}px)`;
+
+    // Update active indicator
+    indicators.forEach((indicator, index) => {
+      indicator.classList.toggle('active', index === currentIndex);
+    });
+  }
+
+  function nextSlide() {
+    if (currentIndex < cardCount - cardPerView) {
+      currentIndex++
+      console.log(currentIndex)
     }
     else {
-        currentIndex = 0
+      currentIndex = 0
     }
-      updateSlider();
-    }
-  
-    function startAutoplay() {
-      setInterval(nextSlide, 3000); // Adjust the interval as needed (in milliseconds)
-    }
-  
-    startAutoplay();
-  });
-  
+    updateSlider();
+  }
+
+  function startAutoplay() {
+    setInterval(nextSlide, 3000); // Adjust the interval as needed (in milliseconds)
+  }
+
+  startAutoplay();
+});
